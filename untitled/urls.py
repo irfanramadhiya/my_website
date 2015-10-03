ad9984efcc8e620ec2f3a7a756b7cd9a55e6cc58
irfanramadhiya <irfan_ramadhiya@.co.id>
2015-10-03 16:15:50 +0700

    refactor project shop

--- untitled/urls.py
+++ untitled/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from catalog import views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'portofolio.views.index', name='index'),
     # url(r'^$', 'untitled.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^home/', views.HomeView.as_view() , name='home'),
+    url(r'^admin/',include(admin.site.urls)),
+    url(r'^about/',views.AboutView.as_view(), name='about'),
+    url(r'^FAQ/',views.FAQView.as_view(), name='FAQ'),
+    url(r'^products/$',views.ProductsView.as_view(), name='products'),
+    url(r'^login/',views.LoginView.as_view(), name='login'),
+    url(r'^register/',views.RegisterView.as_view(), name='register'),
+    url(r'^dashboard/',views.DashboardView.as_view(), name='dashboard'),
+    url(r'^profile/',views.ProfileView.as_view(), name='profile'),
+    url(r'^logout/',views.LogoutView.as_view(), name='logout'),
+    url(r'^products/(?P<id>\d+)/details',views.DetailsView.as_view(id=None), name='product-details'),
+    url(r'^products/add/$',views.AddProductsView.as_view(), name='add-product'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
